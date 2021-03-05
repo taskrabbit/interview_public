bca512f670c528e5215f0941cc62d4b84527912e
Yoshio Varney <yoshio.varney@gmail.com>
2021-03-04 17:06:35 -0800

    Fix functional component

--- login_form/src/components/Link.js
+++ login_form/src/components/Link.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 // The Link component is a simple helper to avoid calling event.preventDefault()
 // every time we want to render a dummy link in our app.
-const Link = () => {
+const Link = (props) => {
   const _handleClick = (event) => {
     event.preventDefault();
-    this.props.onClick();
+    props.onClick();
   };
 
   return (
     // eslint-disable-next-line
     <a href="#" onClick={_handleClick}>
-      {this.props.children}
+      {props.children}
     </a>
   );
 };
